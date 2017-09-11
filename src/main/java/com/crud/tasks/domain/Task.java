13f85b61c8b6d2ac94fd53a6b8287320686af8a9
aleksandramaria <ola.ksi@gmail.com>
2017-09-11 17:55:31 +0200

    tasks

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Created by ola on 11/09/2017.
+ */
+@Getter
+@AllArgsConstructor
+public class Task {
+    private String id;
+    private String title;
+    private String content;
+}
