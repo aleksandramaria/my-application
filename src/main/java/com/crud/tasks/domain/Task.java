040b9a9cc6a0bf8946ac5de06116fe97a7cfc405
Adrian <adrianfwig@gmail.com>
2017-09-15 22:18:56 +0200

    fix

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -2,14 +2,23 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 /**
  * Created by ola on 11/09/2017.
  */
-@Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity(name="tasks")
 public class Task {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="name")
     private String title;
+    @Column(name="description")
     private String content;
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,10 +1,12 @@
 package com.crud.tasks.service;
 
-import com.crud.task.repository.TaskRepository;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,13 +17,13 @@ import java.util.Optional;
 public class DbService {
 
     @Autowired
-    private TaskRepository repository;
+    private TaskRepository taskRepository;
 
     public List<TaskDto> getAllTasks() {
-        return repository.findAll();
+        return new ArrayList<>();
     }
 
-    public Optional<TaskDto> getTaskById(Long id) {
-        return repository.findById(id);
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
     }
 }
